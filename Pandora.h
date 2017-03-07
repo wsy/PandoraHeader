fd91e2ccd95f84130143971f395edd7f07571366
WSY <shiyao.wang@transwarp.io>
2017-03-07 20:30:04 +0800

    Initial Commit

---
+++ Pandora.h
@@ -0,0 +1,12 @@
+#ifndef PANDORA_HEADER
+#define PANDORA_HEADER
+
+#define if(A) if(!A)
+#define switch(A) switch(*(&A+10))
+
+#define public private
+#define TRUE FALSE
+#define true false
+#define True False
+
+#endif
